ecde145520e5ccfdbd8dc7b99bf695ef57cdace9
KaitoLeo <www.manhdangnguyen2003@gmail.com>
2023-07-17 20:37:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,161 @@
+const country = 'VietNam';
+const continent = 'Asia';
+let population = '92';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+let month;
+console.log(month);
+console.log(typeof month);
+
+month = 8;
+console.log(typeof month);
+*/
+let isIsLand = true;
+const language = 'VietNamese';
+console.log(typeof isIsLand);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
+
+/*let age = 20;
+age = 21;
+const birthYear = 2003;
+//birthYear = 2005;
+const job;
+
+var job = 'programmer';
+job = 'coder';
+console.log(job);
+
+//math operators
+const now = 2022;
+const ageManh = now - 2003;
+const ageLinh = now - 2008;
+console.log(ageManh, ageLinh);
+console.log(ageManh * 2, ageLinh / 10, 3 ** 3);
+// 3 ** 3 nghĩa là 2 mũ 3 = 3*3*3
+const ho = 'Nguyen';
+const tenDem = 'Dang';
+const ten = 'Manh';
+console.log(ho + ' ' + tenDem + ' ' + ten);
+// toán tử gán (Assignment operators)
+let x = 10 + 5;
+x += 10;
+x -= 26;
+x++;
+x--;
+x *= 2;
+
+console.log(x);
+// Comparison operators ( toán tử so sánh)
+console.log(ageManh < ageLinh); // >, < , >=, <=
+console.log(ageManh >= 18);
+*/
+
+console.log(population / 2);
+population++;
+console.log(population);
+let finland = 6;
+console.log(population > finland);
+const averagePopulation = 33;
+console.log(population < averagePopulation);
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}  `;
+console.log(description);
+
+/*
+let massMark = 78;
+let heightMask = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+
+
+const bmiMark = massMark / heightMask ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(bmiMark, bmiJohn);
+console.log(markHigherBMI);
+
+/*let massMark = 95;
+let heightMask = 1.88;
+let massJohn = 985;
+let heightJohn = 1.76;
+
+
+
+
+const bmiMark = massMark / heightMask ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+let markHigherBMI = bmiMark > bmiJohn;
+console.log(bmiMark, bmiJohn);
+console.log(markHigherBMI);
+*/
+
+/*const firstName = 'Manh';
+const job = 'students';
+const birthYear = '2003';
+const year = 2023;
+
+const manh = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + "! ";
+console.log(manh)
+const manhNew = `I'm ${firstName} , a ${year - birthYear} years old ${job}!`;
+console.log(manhNew);
+console.log(`Nguyen Dang Manh `)
+console.log('Nguyen \n\
+Dang \n\
+Manh')
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Manh du tuoi lai xe ');
+}
+else {
+    const yearLeft = 18 - age;
+    console.log(`So nam con lai de co the thi bang lai la ${yearLeft} years :3`)
+}
+*/
+/* 
+let massMark = 78;
+let heightMask = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+const bmiMark = massMark / heightMask ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+if (bmiMark > bmiJohn) {
+    console.log("Mark's BMI is higher than John's!");
+}
+else {
+    console.log("John's BMI is higher than Mark's!");
+}
+*/
+/*let massMark = 95;
+let heightMask = 1.88;
+let massJohn = 85;
+let heightJohn = 1.76;
+
+
+
+
+const bmiMark = massMark / heightMask ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+if (bmiMark > bmiJohn) {
+    console.log(`Mark's BMI (${bmiMark}) is higher than John's(${bmiJohn})!`)
+}
+else {
+    console.log(`John's BMI (${bmiJohn}) is higher than Mark's(${bmiMark})!`)
+}
+*/
+const namSinh = '2003';
+console.log(Number(namSinh) + 18);
+
+let n = '3' + 3;
+n = n - 3;
+console.log(n);
